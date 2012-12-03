a3ed4897918a97924f20372e28c50bfd946d5f6a
DY.Feng <yyfeng88625@gmail.com>
2012-12-03 22:59:12 +0800

    delete unnecessary files again

--- wordpress/theme/js/three-slide.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $(".tab_content").hide();
-
-  $("ul.tabs li a").click(function(e) {
-    $(this).closest("li").addClass("active").siblings().removeClass("active");
-    $(this.hash).fadeIn().siblings().hide();
-    e.preventDefault();
-  }).eq(1).click(); //click the second
-});
